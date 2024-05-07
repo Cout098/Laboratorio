ca023093674ce1c6a80b7aca110606f2814fbd65
Breder Couto <104814239+Cout098@users.noreply.github.com>
2024-05-06 21:07:26 -0300

    atividades em sala

---
+++ 07 - Vetores/atividade13.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    int vetor1[10];
+    int vetor2[5];
+    int vetor3[5];
+    int i, j = 0, k = 0, n;
+
+    printf("Digite numeros inteiros para preencher os vetores:\n");
+
+    for (i = 0; i < 10; i++){
+        while(1){
+            printf("Digite o numero: ");
+            scanf("%d", &n);
+
+            if(n >= 0){
+                vetor1[i] = n;
+
+                if (i%2==0){
+                    vetor2[j] = vetor1[i] / 2;
+                    j++;
+                    break;
+                } else {
+                    vetor3[k] = vetor1[i] * 3;
+                    k++;
+                    break;
+                }
+
+            } else {
+                printf("O número digitado é inválido.\n");
+            } 
+        }
+    }
+    
+    printf("\nO primeiro vetor ficara assim: ");
+    for ( i = 0; i < 10; i++)
+    {
+        printf("%d ",vetor1[i]);
+    }
+
+    printf("\nO segundo vetor ficara assim: ");
+    for ( j = 0; j < 5; j++)
+    {
+        printf("%d ",vetor2[j]);
+    }
+
+    printf("\nO terceiro vetor ficara assim: ");
+    for ( k = 0; k < 5; k++)
+    {
+        printf("%d ",vetor3[k]);
+    }
+    
+
+    return 0;
+}
---
+++ 07 - Vetores/atividade14.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+    char gabarito[10] = {'a','a','a','a','a','a','a','a','a','a'};
+    char resposta[10];
+    int acertos = 0, erros = 0, i;
+
+    printf("Digite resposta que foi marcada na prova.\n");
+    for (i = 0; i < 10; i++){
+        printf("Questão %d; ", i+1);
+        scanf(" %c", &resposta[i]);
+    }
+
+    for (i = 0; i < 10; i++)
+    {
+        if (resposta[i]==gabarito[i]){
+           printf("Acertou a questão %d\n", i+1);
+           acertos++;
+        }
+        else {
+            printf("Errou a questão %d\n", i+1);
+            erros++;
+        }
+    }
+    
+    printf("Acertou %d porcento da prova\n", acertos * 10);
+    printf("Errou %d porcento da prova\n", erros * 10);
+
+    return 0;
+}
