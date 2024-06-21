fcae1e42f8c8cee43a7c827948425313455b32b8
Breder Couto <104814239+Cout098@users.noreply.github.com>
2024-06-21 13:28:24 -0300

    Atividades em casa

---
+++ 12 - Typedef/atividade01.c
@@ -0,0 +1,22 @@
+/* 01 – Permita que o usuário cadastre e exiba as informações de um vetor de 4 posições da
+estrutura Empresa (nomeFantasia, razaoSocial e CNPJ) */
+
+#include <stdio.h>
+
+typedef struct 
+{
+    char nomeFantasia[50];
+    char razaoSocial[50];
+    char cnpj[25];
+
+} tp_empresa;
+
+addcadastro
+
+int main(){
+    tp_empresa empresa[4];
+
+    return 0;
+}
+
+
